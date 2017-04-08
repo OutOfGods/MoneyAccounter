6b0aeb84a4a5ad5cf3d24a3e3ea35dfe46b19a33
danylo <fedorough@gmail.com>
2017-04-09 00:59:13 +0300

    load data to table

--- ui_window.py
+++ ui_window.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'gui.ui'
 #
-# Created: Wed Mar 29 03:51:33 2017
+# Created: Sun Apr  9 00:44:02 2017
 #      by: PyQt5 UI code generator 5.2.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -12,40 +12,49 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(679, 385)
+        MainWindow.resize(709, 355)
+        icon = QtGui.QIcon.fromTheme("terminator")
+        MainWindow.setWindowIcon(icon)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setGeometry(QtCore.QRect(0, 0, 671, 336))
+        self.centralwidget.setGeometry(QtCore.QRect(0, 0, 700, 331))
         self.centralwidget.setObjectName("centralwidget")
-        self.table = QtWidgets.QTableView(self.centralwidget)
-        self.table.setGeometry(QtCore.QRect(10, 20, 651, 201))
+        self.table = QtWidgets.QTableWidget(self.centralwidget)
+        self.table.setGeometry(QtCore.QRect(10, 20, 690, 200))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.table.sizePolicy().hasHeightForWidth())
+        self.table.setSizePolicy(sizePolicy)
+        self.table.viewport().setProperty("cursor", QtGui.QCursor(QtCore.Qt.IBeamCursor))
+        self.table.setMouseTracking(False)
+        self.table.setColumnCount(3)
         self.table.setObjectName("table")
-        self.addButton = QtWidgets.QPushButton(self.centralwidget)
-        self.addButton.setGeometry(QtCore.QRect(10, 290, 96, 26))
-        self.addButton.setObjectName("addButton")
-        self.removeButton = QtWidgets.QPushButton(self.centralwidget)
-        self.removeButton.setGeometry(QtCore.QRect(110, 290, 96, 26))
-        self.removeButton.setObjectName("removeButton")
+        self.table.setRowCount(0)
+        self.table.horizontalHeader().setVisible(True)
+        self.table.horizontalHeader().setCascadingSectionResizes(False)
+        self.table.horizontalHeader().setDefaultSectionSize(229)
+        self.table.horizontalHeader().setMinimumSectionSize(100)
         self.amountLine = QtWidgets.QLineEdit(self.centralwidget)
-        self.amountLine.setGeometry(QtCore.QRect(10, 250, 91, 27))
+        self.amountLine.setGeometry(QtCore.QRect(10, 260, 91, 27))
         self.amountLine.setObjectName("amountLine")
         self.dateEdit = QtWidgets.QDateEdit(self.centralwidget)
-        self.dateEdit.setGeometry(QtCore.QRect(550, 250, 110, 27))
+        self.dateEdit.setGeometry(QtCore.QRect(110, 260, 91, 27))
         self.dateEdit.setObjectName("dateEdit")
         self.commentLine = QtWidgets.QLineEdit(self.centralwidget)
-        self.commentLine.setGeometry(QtCore.QRect(210, 250, 331, 27))
+        self.commentLine.setGeometry(QtCore.QRect(210, 260, 481, 27))
         self.commentLine.setObjectName("commentLine")
+        self.addButton = QtWidgets.QPushButton(self.centralwidget)
+        self.addButton.setGeometry(QtCore.QRect(10, 300, 96, 26))
+        self.addButton.setObjectName("addButton")
+        self.removeButton = QtWidgets.QPushButton(self.centralwidget)
+        self.removeButton.setGeometry(QtCore.QRect(110, 300, 96, 26))
+        self.removeButton.setObjectName("removeButton")
         self.tagsLine = QtWidgets.QLineEdit(self.centralwidget)
-        self.tagsLine.setGeometry(QtCore.QRect(110, 250, 91, 27))
+        self.tagsLine.setGeometry(QtCore.QRect(600, 300, 91, 27))
         self.tagsLine.setObjectName("tagsLine")
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(10, 230, 101, 17))
+        self.label.setGeometry(QtCore.QRect(10, 240, 101, 17))
         self.label.setObjectName("label")
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 679, 25))
-        self.menubar.setObjectName("menubar")
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setGeometry(QtCore.QRect(0, 0, 3, 22))
-        self.statusbar.setObjectName("statusbar")
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
@@ -56,14 +65,4 @@ class Ui_MainWindow(object):
         self.addButton.setText(_translate("MainWindow", "add"))
         self.removeButton.setText(_translate("MainWindow", "remove"))
         self.label.setText(_translate("MainWindow", "NEW RECORD"))
-
-
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QDialog()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
 
--- view.py
+++ view.py
@@ -1,10 +1,11 @@
 import sys
-
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from Accounter import *
+from Accounter import Accounter
 from ui_window import Ui_MainWindow
+import pandas as pd
 
 def addButton_clicked():
     print("add")
@@ -12,14 +13,46 @@ def addButton_clicked():
 def removeButton_clicked():
     print("remove")
 
-if __name__== "__main__":
+def load_to_table(ui, acc):
+    acc.load_data()
+    print(acc)
+    
+    ui.table.setHorizontalHeaderItem(0, QTableWidgetItem("how much"))
+    ui.table.setHorizontalHeaderItem(1, QTableWidgetItem("when"))
+    ui.table.setHorizontalHeaderItem(2, QTableWidgetItem("comment"))
+    ui.table.setColumnWidth(0, 100)
+    ui.table.setColumnWidth(1, 100)
+    ui.table.setColumnWidth(2, 500)
+
+    for i, [ n, row ] in enumerate(acc.account.iterrows()):
+        print(i)
+        print(n)
+        print(row['value'])
+        print(row['date'])
+        print(row['comment'])
+        print('----')
+        ui.table.insertRow(ui.table.rowCount())
+        ui.table.setItem(i, 0, QTableWidgetItem(str(row['value'])))
+        ui.table.setItem(i, 1, QTableWidgetItem(row['date'].strftime("%d/%m/%Y")))
+        ui.table.setItem(i, 2, QTableWidgetItem(row['comment']))
+
+def start():
     app = QApplication(sys.argv)
     window = QDialog()
+    QShortcut(QKeySequence("Ctrl+Q"), window, window.close)
+    
     ui = Ui_MainWindow()
     ui.setupUi(window)
 
-    ui.addButton.clicked.connect(addButton_clicked)
-    ui.removeButton.clicked.connect(removeButton_clicked)
+    acc = Accounter()
+    
+    load_to_table(ui, acc)
 
+    ui.addButton.clicked.connect(addButton_clicked)
+    ui.removeButton.clicked.connect(removeButton_clicked)        
+    
     window.show()
     sys.exit(app.exec_())
+
+if __name__== "__main__":
+    start()
