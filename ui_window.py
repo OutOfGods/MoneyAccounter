b70ecbb1531a8a91485067842cdf1fe6686fcbc6
danylo <fedorough@gmail.com>
2017-04-09 20:55:00 +0300

    push

--- ui_window.py
+++ ui_window.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'gui.ui'
 #
-# Created: Sun Apr  9 00:44:02 2017
+# Created: Sun Apr  9 20:34:29 2017
 #      by: PyQt5 UI code generator 5.2.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -12,14 +12,14 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(709, 355)
+        MainWindow.resize(617, 355)
         icon = QtGui.QIcon.fromTheme("terminator")
         MainWindow.setWindowIcon(icon)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setGeometry(QtCore.QRect(0, 0, 700, 331))
+        self.centralwidget.setGeometry(QtCore.QRect(0, 0, 617, 331))
         self.centralwidget.setObjectName("centralwidget")
         self.table = QtWidgets.QTableWidget(self.centralwidget)
-        self.table.setGeometry(QtCore.QRect(10, 20, 690, 200))
+        self.table.setGeometry(QtCore.QRect(10, 20, 600, 200))
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -32,29 +32,30 @@ class Ui_MainWindow(object):
         self.table.setRowCount(0)
         self.table.horizontalHeader().setVisible(True)
         self.table.horizontalHeader().setCascadingSectionResizes(False)
-        self.table.horizontalHeader().setDefaultSectionSize(229)
+        self.table.horizontalHeader().setDefaultSectionSize(100)
         self.table.horizontalHeader().setMinimumSectionSize(100)
+        self.table.verticalHeader().setVisible(True)
         self.amountLine = QtWidgets.QLineEdit(self.centralwidget)
         self.amountLine.setGeometry(QtCore.QRect(10, 260, 91, 27))
         self.amountLine.setObjectName("amountLine")
         self.dateEdit = QtWidgets.QDateEdit(self.centralwidget)
-        self.dateEdit.setGeometry(QtCore.QRect(110, 260, 91, 27))
+        self.dateEdit.setGeometry(QtCore.QRect(110, 260, 121, 27))
         self.dateEdit.setObjectName("dateEdit")
         self.commentLine = QtWidgets.QLineEdit(self.centralwidget)
-        self.commentLine.setGeometry(QtCore.QRect(210, 260, 481, 27))
+        self.commentLine.setGeometry(QtCore.QRect(240, 260, 371, 27))
         self.commentLine.setObjectName("commentLine")
-        self.addButton = QtWidgets.QPushButton(self.centralwidget)
-        self.addButton.setGeometry(QtCore.QRect(10, 300, 96, 26))
-        self.addButton.setObjectName("addButton")
-        self.removeButton = QtWidgets.QPushButton(self.centralwidget)
-        self.removeButton.setGeometry(QtCore.QRect(110, 300, 96, 26))
-        self.removeButton.setObjectName("removeButton")
-        self.tagsLine = QtWidgets.QLineEdit(self.centralwidget)
-        self.tagsLine.setGeometry(QtCore.QRect(600, 300, 91, 27))
-        self.tagsLine.setObjectName("tagsLine")
+        self.push_button = QtWidgets.QPushButton(self.centralwidget)
+        self.push_button.setGeometry(QtCore.QRect(10, 300, 96, 26))
+        self.push_button.setObjectName("push_button")
+        self.pop_button = QtWidgets.QPushButton(self.centralwidget)
+        self.pop_button.setGeometry(QtCore.QRect(110, 300, 96, 26))
+        self.pop_button.setObjectName("pop_button")
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(10, 240, 101, 17))
+        self.label.setGeometry(QtCore.QRect(10, 230, 101, 17))
         self.label.setObjectName("label")
+        self.checkBox = QtWidgets.QCheckBox(self.centralwidget)
+        self.checkBox.setGeometry(QtCore.QRect(110, 230, 151, 22))
+        self.checkBox.setObjectName("checkBox")
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
@@ -62,7 +63,8 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "Accounter"))
-        self.addButton.setText(_translate("MainWindow", "add"))
-        self.removeButton.setText(_translate("MainWindow", "remove"))
+        self.push_button.setText(_translate("MainWindow", "push"))
+        self.pop_button.setText(_translate("MainWindow", "pop"))
         self.label.setText(_translate("MainWindow", "NEW RECORD"))
+        self.checkBox.setText(_translate("MainWindow", "use this date?"))
 
--- view.py
+++ view.py
@@ -6,35 +6,53 @@ from Accounter import *
 from Accounter import Accounter
 from ui_window import Ui_MainWindow
 import pandas as pd
-
-def addButton_clicked():
-    print("add")
-
+    
 def removeButton_clicked():
     print("remove")
+
+def row_to_table(ui, row_num, row_series):
+    ui.table.insertRow(row_num)
+    ui.table.setItem(row_num, 0, QTableWidgetItem(str(row_series['value'])))
+    ui.table.setItem(row_num, 1, QTableWidgetItem(row_series['date'].strftime("%d/%m/%Y")))
+    ui.table.setItem(row_num, 2, QTableWidgetItem(row_series['comment']))
 
 def load_to_table(ui, acc):
     acc.load_data()
     print(acc)
-    
+
+    # do things i didn't find a way to do with qt designer
     ui.table.setHorizontalHeaderItem(0, QTableWidgetItem("how much"))
     ui.table.setHorizontalHeaderItem(1, QTableWidgetItem("when"))
     ui.table.setHorizontalHeaderItem(2, QTableWidgetItem("comment"))
     ui.table.setColumnWidth(0, 100)
     ui.table.setColumnWidth(1, 100)
-    ui.table.setColumnWidth(2, 500)
+    ui.table.horizontalHeader().setSectionResizeMode(2, QHeaderView.Stretch)
+    #ui.table.setColumnWidth(2, 350)
 
-    for i, [ n, row ] in enumerate(acc.account.iterrows()):
-        print(i)
-        print(n)
-        print(row['value'])
-        print(row['date'])
-        print(row['comment'])
-        print('----')
-        ui.table.insertRow(ui.table.rowCount())
-        ui.table.setItem(i, 0, QTableWidgetItem(str(row['value'])))
-        ui.table.setItem(i, 1, QTableWidgetItem(row['date'].strftime("%d/%m/%Y")))
-        ui.table.setItem(i, 2, QTableWidgetItem(row['comment']))
+    # init table
+    for i, [ n, row ] in enumerate(acc.account.iloc[::-1].iterrows()): # костылёк вышел, поцоны
+        row_to_table(ui, i, row)
+        # ui.table.insertRow(ui.table.rowCount())
+        # ui.table.setItem(i, 0, QTableWidgetItem(str(row['value'])))
+        # ui.table.setItem(i, 1, QTableWidgetItem(row['date'].strftime("%d/%m/%Y")))
+        # ui.table.setItem(i, 2, QTableWidgetItem(row['comment']))
+
+def make_push_button_clicked(acc, ui):
+    def push_button_clicked():
+        value=ui.amountLine.text()
+        comment=ui.commentLine.text()
+        acc.add_new_data(value=value, comment=comment)
+        row_to_table(ui, 0, acc.account.iloc[len(acc.account.index) - 1])
+        # ui.table.insertRow(0)
+        # ui.table.setItem(0, 0, QTableWidgetItem(str(value)))
+        # ui.table.setItem(0, 1, QTableWidgetItem(dt.datetime.now().strftime("%d/%m/%Y")))
+        # ui.table.setItem(0, 2, QTableWidgetItem(comment))
+        
+        print("add")
+        print(ui.dateEdit.date().toString())
+        print(acc)
+        print("------")
+    return push_button_clicked
 
 def start():
     app = QApplication(sys.argv)
@@ -48,8 +66,8 @@ def start():
     
     load_to_table(ui, acc)
 
-    ui.addButton.clicked.connect(addButton_clicked)
-    ui.removeButton.clicked.connect(removeButton_clicked)        
+    ui.push_button.clicked.connect(make_push_button_clicked(acc, ui))
+    ui.pop_button.clicked.connect(removeButton_clicked)        
     
     window.show()
     sys.exit(app.exec_())
