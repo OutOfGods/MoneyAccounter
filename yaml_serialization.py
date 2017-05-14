cbc1de21742fb17929b4f720080690ea2e5286c6
Serhiy Latyuk <serhiy_latyuk@inbox.ru>
2017-05-14 11:19:12 +0300

    add yaml serialization

---
+++ yaml_serialization.py
@@ -0,0 +1,32 @@
+import accounter
+import pandas as pd
+import yaml
+
+def serialize(obj, file_name):
+    rows_list = []
+    for i in range(len(obj.account)):
+        row_dict = {}
+        row_dict['comment'] = obj.account.iloc[i]['comment']
+        row_dict['date'] = str(obj.account.iloc[i]['date'])
+        row_dict['value'] = int(obj.account.iloc[i]['value'])
+        rows_list.append(row_dict)
+    with open(file_name, 'w') as f:
+        yaml.dump(rows_list, f)
+
+
+def deserialize(file_name):
+    with open(file_name) as f:
+        obj = yaml.safe_load(f)
+        for record in obj:
+            record['date'] = pd.Timestamp(record['date'])
+        return accounter.Accounter(pd.DataFrame(obj))
+
+
+if __name__ == '__main__':
+    acc = accounter.Accounter()
+    acc.load_data()
+    print(acc)
+    print("")
+    serialize(acc, 'accounter.yaml')
+    acc = deserialize('accounter.yaml')
+    print(acc)
